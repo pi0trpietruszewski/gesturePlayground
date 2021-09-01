bad7f7bbc59bb9984b5bc5590c7cf6589aeed7d9
piotrpietruszewski <p.pietruszewski0@gmail.com>
2021-09-01 13:41:34 +0200

    actions

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,11 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import {TouchableOpacity} from 'react-native';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<TouchableOpacity />);
 });
